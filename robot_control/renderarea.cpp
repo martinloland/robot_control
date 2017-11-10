cffa54fcc1cc362d636bc9ae45e56a204ccb5fab
Martin Loeland <martinloland@gmail.com>
2017-11-10 22:16:37 +0100

    can now draw lines

--- robot_control/renderarea.cpp
+++ robot_control/renderarea.cpp
@@ -1,7 +1,63 @@
 #include "renderarea.h"
+#include <iostream>
+using namespace std;
 
 RenderArea::RenderArea(QWidget *parent)
     : QWidget(parent)
 {
+    setBackgroundRole(QPalette::Base);
+    setAutoFillBackground(true);
+}
 
+void RenderArea::resizeEvent(QResizeEvent *event)
+{
+    render_width = width();
+    render_height = height();
+    update();
+}
+
+void RenderArea::paintEvent(QPaintEvent *e)
+{
+    QPainter painter(this);
+    draw_axis();
+    draw_line(0.0, 0.0, 1.0, 1.0);
+}
+
+void RenderArea::draw_line(double x1, double y1, double x2, double y2){
+    QPainter painter(this);
+    painter.drawLine(render_coord(x1,y1),
+                     render_coord(x2,y2));
+}
+
+void RenderArea::draw_axis(){
+    QPainter painter(this);
+    int margin=axis_margin;
+    QPoint points[3] = {
+        QPoint(margin, margin),
+        QPoint(margin, render_height-margin),
+        QPoint(render_width-margin, render_height-margin)
+    };
+    painter.drawPolyline(points, 3);
+
+    brush = QBrush(Qt::SolidPattern);
+    painter.setBrush(brush);
+
+    int width=10;
+    QPoint arrow1[3] = {
+        QPoint(margin-width/2, margin),
+        QPoint(margin, margin-width),
+        QPoint(margin+width/2, margin)
+    };
+    QPoint arrow2[3] = {
+        QPoint(render_width-margin, render_height-margin-width/2),
+        QPoint(render_width-margin+width, render_height-margin),
+        QPoint(render_width-margin, render_height-margin+width/2)
+    };
+    painter.drawPolygon(arrow1, 3);
+    painter.drawPolygon(arrow2, 3);
+}
+
+QPoint RenderArea::render_coord(double x, double y){
+    return QPoint(x/scaling+axis_margin,
+                  render_height-y/scaling-axis_margin);
 }
--- robot_control/renderarea.h
+++ robot_control/renderarea.h
@@ -2,20 +2,32 @@
 #define RENDERAREA_H
 
 #include <QWidget>
+#include <QPainter>
 
 class RenderArea : public QWidget
 {
     Q_OBJECT
+
 public:
     explicit RenderArea(QWidget *parent = nullptr);
-
-signals:
-
-public slots:
+    void draw_line(double x1, double y1, double x2, double y2);
 
 protected:
+    void paintEvent(QPaintEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+
+    void draw_axis();
+    QPoint render_coord(double x, double y);
+
+    int render_width;
+    int render_height;
+    int axis_margin = 20;
+    double scaling = 1.0/500.0;
 
 private:
+
+    QBrush brush;
+
 };
 
 #endif // RENDERAREA_H
